7607f960957202cd288df24613336cee8acdcb04
walifSub <walifneloy01675>
2016-04-21 09:58:25 +0600

    First time create controller

---
+++ UniversityManagementSystem/Controllers/CourseAssignController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UniversityManagementSystem.Controllers
+{
+    public class CourseAssignController : Controller
+    {
+        // GET: CourseAssign
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
